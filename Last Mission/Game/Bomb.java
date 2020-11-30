5d2ac934bb190d5344622bf934576f4a6fa49de5
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-11-30 21:44:56 +0900

    initBomb Edit

--- Last Mission/Game/Bomb.java
+++ Last Mission/Game/Bomb.java
@@ -6,17 +6,16 @@ public class Bomb{
     int x= 0;
     int y= 0;
 
-    public void initBomb(Monster m) {
+
+    public void initBomb() {
         while (true) {
-            x= random.nextInt(10);
-            y= random.nextInt(10);
+            x= random.nextInt(11);
+            y= random.nextInt(11);
 
             if (x != 5 && y != 5) {
-                if(x != m.x && y!= m.y){
-
-                }
                 break;
             }
+
         }
 
     }
