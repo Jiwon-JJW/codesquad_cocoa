ac3d09c3f520004d4521aeb2f1b58bcd8fb9b808
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-11-30 20:12:06 +0900

    Unit Upload

---
+++ Last Mission/Game/Unit.java
@@ -0,0 +1,31 @@
+public class Unit {
+    protected int x;
+    protected int y;
+
+    private int score;
+
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
