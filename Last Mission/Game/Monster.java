10819645650d788b67d039c82e89fe67e67cedc8
Jiwon-JJW <69128652+Jiwon-JJW@users.noreply.github.com>
2020-11-30 21:45:30 +0900

    initMonster Edit

--- Last Mission/Game/Monster.java
+++ Last Mission/Game/Monster.java
@@ -2,15 +2,14 @@ import java.util.Random;
 
 public class Monster {
     Random random = new Random();
-    Bomb b = new Bomb();
 
     int x= 0;
     int y= 0;
 
     public void initMonster() {
         while (true) {
-            x=random.nextInt(10);
-            y=random.nextInt(10);
+            x=random.nextInt(11);
+            y=random.nextInt(11);
 
             if (x != 5 && y != 5) {
                 break;
